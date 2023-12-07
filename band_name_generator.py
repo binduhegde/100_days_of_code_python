a53a9e154d20cfaeb3f4ea2a142902a89eac77b3
Bindu Hegde <binduhegde1932@gmail.com>
2023-12-07 17:10:30 +0530

    create band_name_generator

---
+++ band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator!")
+city = input("Enter the name of the city you grew up in?\n")
+pet = input("Enter the name of your first pet?\n")
+print(f"Your band could be {city} {pet}")
