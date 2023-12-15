7db45876c51f2fd64fad516a5296c344b852fdcb
Bindu Hegde <binduhegde1932@gmail.com>
2023-12-15 11:20:10 +0530

    make 8_caesar_cipher better

--- 8_caesar_cipher.py
+++ 8_caesar_cipher.py
@@ -17,47 +17,59 @@ a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
               88           
 ''')
     
-def get_inputs():
-    enc_or_dec = 'p'
-    while enc_or_dec not in ['e', 'd']:
-        enc_or_dec = input("Type 'e' to encrypt, type 'd' to decrypt: ")
-    
-    string = input("Type your message: ")
-    
-    number = ''
-    while type(number) is not int:
+def get_operation_type():
+    while True:
+        operation_type = input("Type 'e' to encrypt, type 'd' to decrypt: ")
+        if operation_type in ['e', 'd']:
+            return operation_type
+        else:
+            print("Invalid input. Please type 'e' to encrypt or 'd' to decrypt.")
+
+def get_message():
+    return input("Type your message: ")
+
+def get_shift_amount():
+    while True:
         try:
-            number = int(input("Type the shift number: "))
-        except:
-            continue
+            shift_amount = int(input("Type the shift number: "))
+            return shift_amount
+        except ValueError:
+            print("Invalid input. Please enter a valid integer for the shift.")
 
-    return [enc_or_dec, string, number]
-
-def encode(string, number):
+def encrypt(message, shift):
     result = ''
-    for char in string:
-        order = ord(char)
-        new_order = order + number
-        while new_order > 122:
-            new_order -= 26
-        result += chr(new_order)
+    for char in message:
+        if char.isalpha():
+            offset = 65 if char.isupper() else 97
+            
+            # 'ord(char) - offset': ord(char) - 65 if it's uppper else 97
+            # 'ord(char) - offset + shift': Applies the shift value to the character position. 
+            # '(ord(char) - offset + shift) % 26': to make sure for the shift value + char 
+            #       doesn't go beyond the letter z. it wrapes around
+            # '(ord(char) - offset + shift) % 26 + offset': Returns the shifted character's 
+            #       position back to the ascii value within the alphabet range.
+            # 'chr((ord(char) - offset + shift) % 26 + offset)': Converts the shifted ascii 
+            #       value back to the corresponding character.
+            result += chr((ord(char) - offset + shift) % 26 + offset)
+        else:
+            result += char # doesn't encode or decode anything other than alphabet
     return result
 
-def decode(string, number):
-    result = ''
-    for char in string:
-        order = ord(char)
-        new_order = order - number
-        while new_order < 97:
-            new_order += 26
-        result += chr(new_order)
-    return result
+def decrypt(message, shift):
+    return encrypt(message, -shift)
 
-def run():
+def run_cipher():
     print_logo()
-    inputs = get_inputs()
-    if inputs[0] == 'e':
-        print(f"Encoded message is: {encode(inputs[1], inputs[2])}")
+    
+    operation_type = get_operation_type()
+    message = get_message()
+    shift = get_shift_amount()
+
+    if operation_type == 'e':
+        encrypted_message = encrypt(message, shift)
+        print(f"Encoded message is: {encrypted_message}")
     else:
-        print(f"Decoded message is: {decode(inputs[1], inputs[2])}")
-run()
+        decrypted_message = decrypt(message, shift)
+        print(f"Decoded message is: {decrypted_message}")
+
+run_cipher()
